0615cc086d4479262c2317bfe89b6140cf559e27
Ashley Taylor <blueberrypea@gmail.com>
2023-05-18 10:45:04 -0400

    initital lab 06 sestup

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Form, Button } from 'react-boostrap';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityName: ''
+    }
+  }
+
+  handleCitySubmit = (event) => {
+    event.preventDefault();
+    
+   
+  }
+
+  changeCityInput = (event) => {
+    this.setState({
+      cityName: event.target.value
+    });
+  }
+ 
+  render() {
+    return (
+    <>
+      <h1>City Explorer</h1>;
+      <Form onSubmit={this.handleCitySubmit}>
+        <Form.Label>Search for a City:
+          <Form.Control onChange={this.changeCityInput}/>
+        </Form.Label>
+        <
+      </Form>
+    </>
+    );
+  }
+}
+
+export default App;
